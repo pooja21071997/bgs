af374ddd0f45d5660f2fa3799f0f3d8faa57d298
poojaPersonal <100690066+pooja21071997@users.noreply.github.com>
2026-01-30 15:30:43 +0530

    facilities page changes

--- src/app/pages/academics/academics.ts
+++ src/app/pages/academics/academics.ts
@@ -1,11 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 @Component({
   selector: 'app-academics',
-  imports: [],
+  imports: [PdfViewerModule,CommonModule],
   templateUrl: './academics.html',
   styleUrl: './academics.css',
 })
 export class Academics {
+pdfSrc = '/assets/files/Diary-2025-26.pdf';
+//pdfSrc = 'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf';
+
+page = 1;
+totalPages = 0;
+zoom = 0.6;
+
+afterLoad(pdf: any) {
+  this.totalPages = pdf.numPages;
+  this.fitToPage();
+}
+
+fitToPage() {
+  // Adjust zoom for screen size (trial-tested values)
+  const screenWidth = window.innerWidth;
+
+  if (screenWidth < 768) {
+    this.zoom = 0.6;
+  } else if (screenWidth < 1200) {
+    this.zoom = 0.8;
+  } else {
+    this.zoom = 0.6;
+  }
+}
+
+nextPage() {
+  if (this.page < this.totalPages) this.page++;
+}
+
+prevPage() {
+  if (this.page > 1) this.page--;
+}
 
 }
